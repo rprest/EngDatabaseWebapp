b806aa6a34d29478a8cec3ca64ee891df1aafc86
rprest <ryan@prestonweb.com>
2025-11-12 02:03:37 -0600

    added reactive review property tag

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [pageID, setPageID] = useState('Page ID');
   const [pageURL, setPageURL] = useState('');
   const [pageIDclean, setPageID_clean] = useState('');
+  const [NeedToReview, setNeedToReview] = useState(false);
 
   const fetchRecentPage = async () => {
     const response = await fetch('http://localhost:5000/api/recentpage');
@@ -17,18 +18,19 @@ function App() {
     setPageID(data.page_id);
     setPageURL(data.notion_url);
     setPageID_clean(data.page_id_clean);
+    setNeedToReview(data.checkbox_properties["Need to Review"]);
   };
 
   return (
     <>
       <button onClick={fetchRecentPage} className="w-full mb-3 py-1 border-white text-white border-2 rounded-xl px-2 hover:shadow-md/40 shadow-white hover:ring-1 hover:cursor-pointer">Refresh</button>
 
-      <a href="https://www.notion.so/Adiabatic-Process-2978ecb0c9a5807aba90cbaa223053dc?v=2528ecb0c9a5800a85a3000c6d9a863f&" className="block px-5 border-white text-white border-2 rounded-xl p-3 hover:shadow-lg/40 shadow-white hover:ring-1 text-left hover:cursor-pointer no-underline">
+      <a href={pageURL} className="block px-5 border-white text-white border-2 rounded-xl p-3 hover:shadow-lg/40 shadow-white hover:ring-1 text-left hover:cursor-pointer no-underline">
         
         <h1 className="text-5xl font-bold">{message}</h1>
         <p className="py-2">{pageID}</p>
-        <div className="bg-amber-400 rounded inline-block">      
-          <p className="text-black px-2">Tag</p>
+        <div className={`${NeedToReview ? 'bg-red-900' : 'bg-green-900'} rounded inline-block`}>      
+          <p className="text-white px-2">{NeedToReview ? 'Needs Review' : 'No Need For Review'}</p>
         </div>
         
       </a>
