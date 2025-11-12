4eba9e00fcae4ac92a7a290df97757bb0d48b013
rprest <ryan@prestonweb.com>
2025-11-11 23:52:13 -0600

    more css

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="border-2 rounded-xl p-3 hover:shadow-lg/40 shadow-white hover:ring-1 text-left hover:cursor-pointer">
+      
+      <h1 className="text-3xl font-bold">Test Card</h1>
+      <p className="p-2">This is more text</p>
+      <div className="bg-amber-400 rounded inline-block">      
+        <p className="text-black px-2">Tag</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
